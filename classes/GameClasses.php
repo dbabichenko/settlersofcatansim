50c36d3a7a85450407b3179c21aaefb0ba323756
Chong Zhang <zigchg@gmail.com>
2016-10-25 10:14:37 -0400

    updates

--- classes/GameClasses.php
+++ classes/GameClasses.php
@@ -8,6 +8,15 @@
     $numPlayers;
     $banditLocation;
 
+    function debug_to_console( $data ) {
+        if ( is_array( $data ) )
+            $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+        else
+            $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+
+        echo $output;
+    }
+
     class Game {
         public $color = array();
         public $currentPlayer;
